75a9b5552844724e3edee5f0addab650c8e3bbce
usamakhan04 <usamakhan04@gmail.com>
2021-12-20 15:07:22 +0500

    updated

---
+++ src/ContectUS.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class ContectUS extends Component {
+    render() {
+        return (
+            <div>
+                 <section className="hero-section ptb-100 gradient-overlay background" style={{background: 'url("img/contect\ us.jpg")  no-repeat center center / cover'}}>
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-8 col-lg-7">
+        <div className="page-header-content text-white  pt-sm-5 pt-md-5 pt-lg-0">
+          <h1 className=" mb-0">Contact Us</h1>
+          <div className="custom-breadcrumb">
+            <h5>Contect &amp; Email</h5>
+            <h5 className="text-white"><i className="fa fa-envelope icon-1 text-danger" />info@theassignmentwriting.com</h5>
+            <h5 className="text-white"><i className="fa fa-envelope icon-1 text-danger" />theassignmentwriting12@gmail.com</h5>
+            <h5 className="text-white"> <i className="fa fa-phone icon-1 text-danger" /> 0018883526601 US toll free US</h5>
+            <h5 className="text-white"><i className="fa fa-phone icon-1 text-danger" />00448000418794 UK toll free UK</h5>
+            <h5 className="text-white"><i className="fa fa-phone icon-1 text-danger"> </i>00447418343231 UK Phone number</h5>
+            <h5 className="text-white"><i className="fa fa-phone icon-1 text-danger" />0016469165519 US phone number</h5>
+            <h5 className="text-white"><i className="fa fa-phone icon-1 text-danger" />00447429125487 UK whatsapp number</h5>
+            <h5 className="text-white"><i className="fa fa-phone icon-1 text-danger" />0015167383453 US whatsapp number</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+            </div>
+        )
+    }
+}
+
+export default ContectUS
